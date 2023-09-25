70b8b10f4435ab2c8a43f2dcb1a8d1e16b577324
evgen <chernous.eu@gmail.com>
2023-09-25 16:22:03 +0300

    parallax

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -1,76 +1,31 @@
 import Swiper from 'swiper/bundle';
 
-const swiper = () => new Swiper('.image-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+const swiper = () => new Swiper('.page', {
+  wrapperClass: "page__wrapper",
+  slideClass: "page__screen",
+  direction: "vertical",
+  slidesPerView: 'auto',
+  parallax: true,
+  mousewheel: {
+    sensitivity: 1,
   },
+  watchOverflow: true,
+  speed: 800,
+  observer: true,
+  observeParents: true,
+  observeSlideChildrens: true,
   pagination: {
-    el: '.swiper-pagination',
-    // clickable: true,
-    // dynamicBullets: true,
-    // type: 'fraction',
-    type: 'progressbar'
+    el: '.page__pagination',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'page__bullet',
+    bulletActiveClass: 'page__bullet_active',
   },
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: '.page__scroll',
+    dragClass: 'page__drag-scroll',
     draggable: true
-  },
-  simulateTouch: true,
-  autoHeight: false,
-  slidesPerView: 2,
-  //Отключение навигации если слайдов меньше чем указано
-  watchOverflow: true,
-  spaceBetween: 30,
-  // slidesPerGroup: 2,
-  centeredSlides: false,
-  // initialSlide: 0,
-  // grid: {
-  //   rows: 2
-  // }
-  loop: false,
-  freeMode: true,
-  autoplay: {
-    delay: 1000,
-    stopOnLastSlide: false,
-    disableOnInteraction: false
-  },
-  speed: 500,
-  // direction: 'vertical'
-  // effect: 'fade',
-  // fadeEffect: {
-  //   crossFade: true,
-  // }
-  // effect: 'flip',
-  // flipEffect: {
-  //   slideShadows: true,
-  //   limitRotation: true 
-  // }
-  // effect: 'cube',
-  // cubeEffect: {
-  //   slideShadows: true,
-  //   shadow: true,
-  //   shadowOffset: 20,
-  //   shadowScale: 0.8
-  // }
-  // effect: 'coverflow',
-  // coverflowEffect: {
-  //   rotate: 20,
-  //   stretch: 50,
-  //   slideShadows: true
-  // }
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    480: {
-      slidesPerView: 2,
-    },
-    992: {
-      slidesPerView: 3,
-    },
-  },
-  
+  }
 });
 
 swiper();
